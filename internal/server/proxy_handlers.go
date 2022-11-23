7adcce2020dcbf80bbdd5c8872e86df749570252
Prabesh Thapa <sthapaprabesh2020@gmail.com>
2022-11-23 15:37:58 -0800

    Working rp

--- internal/server/proxy_handlers.go
+++ internal/server/proxy_handlers.go
@@ -1,19 +1,20 @@
 package server
 
-import (
-	"net/http"
-	"net/http/httputil"
-	"net/url"
-)
+// import (
+// 	"net/http"
+// 	"net/http/httputil"
+// 	"net/url"
+// )
 
-// NewProxy takes target host and creates a reverse proxy
-func NewProxy(url *url.URL) (*httputil.ReverseProxy, error) {
-	return httputil.NewSingleHostReverseProxy(url), nil
-}
+// // NewProxy takes target host and creates a reverse proxy
+// func NewProxy(target string) (*httputil.ReverseProxy, error) {
+// 	url, _ := url.Parse(target)
+// 	return httputil.NewSingleHostReverseProxy(url), nil
+// }
 
 // ProxyRequestHandler handles the http request using proxy
-func ProxyRequestHandler(proxy *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		proxy.ServeHTTP(w, r)
-	}
-}
+// func ProxyRequestHandler(proxy *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		proxy.ServeHTTP(w, r)
+// 	}
+// }
--- internal/server/server.go
+++ internal/server/server.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
 	"net/url"
+	"strings"
 
 	"github.com/pgaijin66/lightweight-reverse-proxy/internal/configs"
 )
@@ -14,6 +16,27 @@ func ping(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("pong"))
 }
 
+// type Prox struct {
+// 	target *url.URL
+// 	proxy  *httputil.ReverseProxy
+// }
+
+// func NewProxy(target string) *Prox {
+// 	url, _ := url.Parse(target)
+// 	return &Prox{
+// 		target: url,
+// 		proxy:  httputil.NewSingleHostReverseProxy(url)}
+
+// }
+
+// func ProxyRequestHandler(proxy *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		fmt.Println("here")
+// 		r.Host = "localhost:9001"
+// 		proxy.ServeHTTP(w, r)
+// 	}
+// }
+
 // Run starts server and listens on defined port
 func Run() error {
 	// load configurations from config file
@@ -22,28 +45,75 @@ func Run() error {
 		log.Fatalf("could not load configuration: %v", err)
 	}
 
-	// creating a new router
-	router := http.NewServeMux()
+	fmt.Println(config.Resources)
 
-	// health check
-	router.HandleFunc("/ping", ping)
+	mux := http.NewServeMux()
 
-	// Iterating over the hosts
-	for _, resources := range configs.Config.Resources {
-		URL, err := url.Parse(configs.Config.Resources[0].Destination_URL)
-		if err != nil {
-			log.Fatal(err)
-		}
-		proxy, _ := NewProxy(URL)
-		router.HandleFunc(resources.Endpoint, ProxyRequestHandler(proxy))
-	}
+	mux.HandleFunc("/", handleRequestAndRedirect1)
+	mux.HandleFunc("/server2", handleRequestAndRedirect2)
+	mux.HandleFunc("/server3", handleRequestAndRedirect3)
 
-	fmt.Println("Starting and Listening server at port: ", config.Server.Listen_port)
-	switch config.Server.Scheme {
-	case "http":
-		http.ListenAndServe(config.Server.Host+":"+config.Server.Listen_port, router)
-	default:
-		return fmt.Errorf("server scheme not supported")
-	}
+	http.ListenAndServe(":8080", mux)
+
 	return nil
+
+}
+
+// Given a request send it to the appropriate url
+func handleRequestAndRedirect1(res http.ResponseWriter, req *http.Request) {
+	// We will get to this...
+	url, _ := url.Parse("http://localhost:9001")
+	// create the reverse proxy
+	proxy := httputil.NewSingleHostReverseProxy(url)
+
+	// Update the headers to allow for SSL redirection
+	req.URL.Host = url.Host
+	req.URL.Scheme = url.Scheme
+	req.Header.Set("X-Forwarded-Host", req.Header.Get("Host"))
+	req.Host = url.Host
+
+	// Note that ServeHttp is non blocking and uses a go routine under the hood
+	proxy.ServeHTTP(res, req)
+}
+
+// Given a request send it to the appropriate url
+func handleRequestAndRedirect2(res http.ResponseWriter, req *http.Request) {
+	// We will get to this...
+	url, _ := url.Parse("http://localhost:9002")
+	// create the reverse proxy
+	proxy := httputil.NewSingleHostReverseProxy(url)
+
+	// Update the headers to allow for SSL redirection
+	req.URL.Host = url.Host
+	req.URL.Scheme = url.Scheme
+	req.Header.Set("X-Forwarded-Host", req.Header.Get("Host"))
+	req.Host = url.Host
+
+	//trim reverseProxyRoutePrefix
+	path := req.URL.Path
+	req.URL.Path = strings.TrimLeft(path, "/server2")
+
+	// Note that ServeHttp is non blocking and uses a go routine under the hood
+	proxy.ServeHTTP(res, req)
+}
+
+// Given a request send it to the appropriate url
+func handleRequestAndRedirect3(res http.ResponseWriter, req *http.Request) {
+	// We will get to this...
+	url, _ := url.Parse("http://localhost:9003")
+	// create the reverse proxy
+	proxy := httputil.NewSingleHostReverseProxy(url)
+
+	// Update the headers to allow for SSL redirection
+	req.URL.Host = url.Host
+	req.URL.Scheme = url.Scheme
+	req.Header.Set("X-Forwarded-Host", req.Header.Get("Host"))
+	req.Host = url.Host
+
+	//trim reverseProxyRoutePrefix
+	path := req.URL.Path
+	req.URL.Path = strings.TrimLeft(path, "/server3")
+
+	// Note that ServeHttp is non blocking and uses a go routine under the hood
+	proxy.ServeHTTP(res, req)
 }

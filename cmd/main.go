ce8eaed26c5d3cdb3499171d579fec5d9de03a21
Prabesh Thapa <sthapaprabesh2020@gmail.com>
2026-04-09 10:35:55 -0700

    perf: set GOMAXPROCS to match available CPU cores

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"log"
+	"runtime"
 
 	"github.com/pgaijin66/tinyrp/internal/server"
 )
 
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
 	if err := server.Run(); err != nil {
 		log.Fatalf("could not start the server: %v", err)
 	}

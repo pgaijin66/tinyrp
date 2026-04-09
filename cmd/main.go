fce87b556b07dc84d17f4bf6dbd40caaca1c3182
Prabesh Thapa <sthapaprabesh2020@gmail.com>
2026-04-09 10:56:47 -0700

    perf(gc): set GOGC=200 to halve GC frequency

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 	"runtime"
+	"runtime/debug"
 
 	"github.com/pgaijin66/tinyrp/internal/server"
 )
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
+	debug.SetGCPercent(200)
 
 	if err := server.Run(); err != nil {
 		log.Fatalf("could not start the server: %v", err)
